895ee9faaff801de758dc7c9609e19191809569a
i201624446 (Temoche Clavero,Giovanni Ricardo) <i201624446@cibertec.edu.pe>
2018-10-31 17:02:46 -0500

    commit

--- Temple/Controllers/AlumnoController.cs
+++ Temple/Controllers/AlumnoController.cs
@@ -91,6 +91,44 @@ namespace Temple.Controllers
 
         }
 
+        private List<TarjetaInstructor> ListadoInstructoresBusqueda(int idCat = 0, int idSub=0)
+        {
+                List<TarjetaInstructor> lista = new List<TarjetaInstructor>();
+                con.Open();
+                
+                SqlCommand cmd = new SqlCommand("USP_OBTENER_INSTRUCTORES_BUSQUEDA", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                
+                cmd.Parameters.AddWithValue("@IDCAT", idCat);
+                cmd.Parameters.AddWithValue("@IDSUB", idSub);
+                SqlDataReader reader = cmd.ExecuteReader();
+                
+                while (reader.Read())
+                {
+
+                    TarjetaInstructor i = new TarjetaInstructor();
+                    i.codigo = reader.GetInt32(0);
+                    i.nombres = reader.GetString(1);
+                    i.apPaterno = reader.GetString(2);
+                    i.apMaterno = reader.GetString(3);
+                    i.especialidad = reader.GetString(4);
+                    i.distancia = reader.GetString(5);
+                    i.numResenas = reader.GetInt32(6);
+                    i.calificacion = reader.GetInt32(7);
+                    i.verificado = reader.GetBoolean(8);
+
+                    lista.Add(i);
+
+                }
+                reader.Close();
+
+            
+            con.Close();
+
+            return lista;
+
+        }
+
         public List<Categoria> ListadoCategorias() {
 
             List<Categoria> lista = new List<Categoria>();
@@ -139,12 +177,31 @@ namespace Temple.Controllers
 
         }
 
-        public ActionResult Inicio()
+        /*[HttpPost]public ActionResult Inicio(int cboCategoria=0, int cboSubcategoria=0) {
+
+            return View();
+
+        }
+        */
+
+        public ActionResult Inicio(int cboCategoria = -1, int cboSubcategoria = -1)
         {
+
+            if (cboCategoria == -1) {
+
+                cboCategoria = ListadoCategorias().ElementAt(0).id;
+
+            }
+
+            if (cboSubcategoria == -1) {
+
+                cboSubcategoria = ListadoSubcategorias(cboCategoria).ElementAt(0).id;
+            }
+
             ViewBag.usuario = Session["usuario"];
             ViewBag.recomendados = ListadoInstructoresRecomendados();
-            ViewBag.categorias = new SelectList(ListadoCategorias(), "id", "descripcion", 0);
-                ListadoCategorias();
+            ViewBag.categorias = new SelectList(ListadoCategorias(), "id", "descripcion", cboCategoria);
+            ViewBag.subcategorias = new SelectList((ListadoSubcategorias(cboCategoria)), "id", "descripcion", cboSubcategoria);
             return View();
 
         }
