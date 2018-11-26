26c838fee52dbab7ead9782cf10ca652e88325a7
i201624446 (Temoche Clavero,Giovanni Ricardo) <i201624446@cibertec.edu.pe>
2018-11-26 00:41:29 -0500

    abuelita

---
+++ Temple/Content/scriptRegistro.js
@@ -0,0 +1,281 @@
+﻿
+// Toolbar extra buttons
+var btnFinish = $('<button></button>')
+    .text('Presentar')
+    .addClass('btn btn-info')
+    .on(
+        'click',
+        function () {
+
+            if (!$(this).hasClass('disabled')) {
+                var elmForm = $("#myForm");
+                if (elmForm) {
+                    elmForm
+                        .validator('validate');
+                    var elmErr = elmForm
+                        .find('.has-error');
+                    if (elmErr
+                        && elmErr.length > 0) {
+                        alert('Oops we still have error in the form');
+                        return false;
+                    } else {
+                        alert('Great! we are ready to submit form');
+                        elmForm.submit();
+                        return false;
+                    }
+                }
+            }
+        });
+var btnCancel = $('<button></button>').text('Cancelar')
+    .addClass('btn btn-danger').on(
+        'click',
+        function () {
+
+            $('#smartwizard').smartWizard(
+                "reset");
+            $('#myForm')
+                .find("input, textarea")
+                .val("");
+        });
+
+// Smart Wizard
+$('#smartwizard').smartWizard({
+    selected: 0,
+    theme: 'arrows',
+    transitionEffect: 'fade',
+    toolbarSettings: {
+        toolbarPosition: 'bottom',
+        toolbarExtraButtons: [
+            btnFinish, btnCancel],
+        cycleSteps: false, // Allows to
+        // cycle the
+        // navigation of
+        // steps
+        backButtonSupport: true
+        // Enable the back button support
+
+    },
+    anchorSettings: {
+        markDoneStep: true, // add done
+        // css
+        markAllPreviousStepsAsDone: true, // When
+        // a
+        // step
+        // selected
+        // by
+        // url
+        // hash,
+        // all
+        // previous
+        // steps
+        // are
+        // marked
+        // done
+        removeDoneStepOnNavigateBack: true, // While
+        // navigate
+        // back
+        // done
+        // step
+        // after
+        // active
+        // step
+        // will
+        // be
+        // cleared
+        enableAnchorOnDoneStep: true
+        // Enable/Disable the done steps
+        // navigation
+    },
+    lang: { // Language variables
+        next: 'Continuar',
+        previous: 'Retroceder'
+    },
+
+    toolbarSettings: {
+        toolbarPosition: 'bottom', // none,
+        // top,
+        // bottom,
+        // both
+        toolbarButtonPosition: 'right', // left,
+        // right
+        showNextButton: true, // show/hide
+        // a Next
+        // button
+        showPreviousButton: true, // show/hide
+        // a
+        // Previous
+        // button
+        toolbarExtraButtons: [
+            $('<button></button>').text('Presentar').addClass('btn btn-info btn-finish d-none')
+                .on('click', function () {
+
+                    //Si acepta los términos y condiciones
+                    var acepta = $(
+                        "#chkAcepta")
+                        .is(
+                            ":checked");
+
+                    if (acepta == false) {
+
+                        alert("Debe aceptar los términos y condiciones");
+
+                    } else {
+
+
+
+                    }
+
+
+
+
+
+
+                }),
+            $('<button></button>').text('Cancelar').addClass('btn btn-danger')
+                .on('click', function () {
+
+                })
+        ]
+    },
+
+    anchorSettings: {
+        anchorClickable: true, // Enable/Disable
+        // anchor
+        // navigation
+        enableAllAnchors: true, // Activates
+        // all
+        // anchors
+        // clickable
+        // all
+        // times
+        markDoneStep: true, // add done
+        // css
+        enableAnchorOnDoneStep: true
+        // Enable/Disable the done steps
+        // navigation
+    },
+    contentURL: null, // content url,
+    // Enables Ajax
+    // content loading.
+    // can set as data
+    // data-content-url
+    // on anchor
+    disabledSteps: [], // Array Steps
+    // disabled
+    errorSteps: [], // Highlight step
+    // with errors
+    transitionSpeed: '400'
+
+});
+
+$("#smartwizard")
+    .on(
+        "leaveStep",
+        function (e, anchorObject, stepNumber,
+            stepDirection) {
+
+            var elmForm = $("#form-step-"
+                + stepNumber);
+            // stepDirection === 'forward' :- this
+            // condition allows to do the form
+            // validation
+            // only on forward navigation, that
+            // makes easy navigation on backwards
+            // still do the validation when going
+            // next
+            if (stepDirection === 'forward'
+                && elmForm) {
+                elmForm.validator('validate');
+                var elmErr = elmForm
+                    .children('.has-error');
+
+                if (elmErr && elmErr.length > 0) {
+                    alert("Debe rellenar todos los campos primero");
+
+                    return false;
+
+                }
+
+            }
+
+            return true;
+        });
+
+$("#smartwizard").on("showStep", function (e, anchorObject, stepNumber, stepDirection) {
+    // Enable finish button only on last
+    // step
+    if (stepNumber == 3) {
+        $('.btn-finish').removeClass('d-none');
+    } else {
+        $('.btn-finish').addClass('d-none');
+    }
+});
+
+// Para agregar los cuadros de preferencia
+
+$('#btnPreferencia')
+    .click(
+        function () {
+
+            var first = $('.fieldPreferencia')
+                .first();
+
+            var last = $('.fieldPreferencia')
+                .last();
+
+            var cloned = $(first).clone(true);
+
+            cloned.css('opacity', '0.1');
+            cloned.insertAfter($(last));
+            $(cloned)
+                .slideDown(
+                    150,
+                    function () {
+                        $(cloned)
+                            .fadeTo(
+                                500,
+                                1);
+                    });
+            // Agrego boton cerrar
+
+            var last = $('.fieldPreferencia').last();
+
+            $(last).prepend('<button type="button" class="close cerrarPreferencia" aria-label="Close">   <span aria-hidden="true">&times;</span> </button>');
+            $(last).find('legend').text("¿Qué más te gustaría aprender?");
+
+            $(last).on('click', 'button.cerrarPreferencia', function () {
+
+                $(last).fadeTo(
+                    500,
+                    0.01,
+                    function () {
+                        $(
+                            last)
+                            .slideUp(
+                                150,
+                                function () {
+                                    $(
+                                        last)
+                                        .remove();
+                                });
+                    });
+                if ($('.fieldPreferencia').length <= 3) {
+                    $(
+                        '#btnPreferencia')
+                        .prop(
+                            'disabled',
+                            false);
+
+                }
+
+            });
+
+            // Verifico si excedio el limite
+
+            if ($('.fieldPreferencia').length >= 3) {
+
+                $(this).prop('disabled', true);
+
+            }
+
+        });
--- Temple/Controllers/RegistroController.cs
+++ Temple/Controllers/RegistroController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using Temple.Models;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace Temple.Controllers
 {
     public class RegistroController : Controller
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+
         // GET: Registro
         public ActionResult RegistroAlumno()
         {
             ViewBag.usuario = Session["usuario"];
+            ViewBag.categorias = new SelectList(ListadoCategorias(), "id", "descripcion", 0);
+            ViewBag.subcategorias = new SelectList((ListadoSubcategorias(ListadoCategorias().ElementAt(0).id)), "id", "descripcion", 0);
 
             return View();
         }
@@ -20,6 +28,57 @@ namespace Temple.Controllers
         public ActionResult RegistroAlumno(int idPref) {
 
             return View();
+
+        }
+
+        public List<Categoria> ListadoCategorias()
+        {
+
+            List<Categoria> lista = new List<Categoria>();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("USP_OBTENER_CATEGORIAS", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+
+                Categoria c = new Categoria();
+                c.id = reader.GetInt32(0);
+                c.descripcion = reader.GetString(1);
+
+                lista.Add(c);
+
+            }
+            reader.Close();
+            con.Close();
+
+            return lista;
+
+        }
+
+        public List<Subcategoria> ListadoSubcategorias(int idCat)
+        {
+
+            List<Subcategoria> lista = new List<Subcategoria>();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("USP_OBTENER_SUBCATEGORIAS", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@IDCAT", idCat);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+
+                Subcategoria c = new Subcategoria();
+                c.id = reader.GetInt32(0);
+                c.descripcion = reader.GetString(1);
+
+                lista.Add(c);
+
+            }
+            reader.Close();
+            con.Close();
+
+            return lista;
 
         }
 
